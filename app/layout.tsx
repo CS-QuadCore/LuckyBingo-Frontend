73fa4233d5dd8fbfec8095aff9cab2a747e39025
JESREAL1JDL7LUSTRE <jesreal.lustre@yahoo.com>
2026-05-05 22:07:01 +0800

    feat(called-numbers): collapsible inline previous numbers (right) and hydration fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
       lang="en"
       className={`${fraunces.variable} ${dmSans.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body suppressHydrationWarning className="min-h-full flex flex-col" data-gr-ext-installed="">{children}</body>
     </html>
   );
 }
